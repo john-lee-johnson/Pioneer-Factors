a4c2a37ff19aa0a229ea0ff337dcad4be7509350
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-07 11:39:28 -0400

    changed dir

--- chip-seq.sh
+++ chip-seq.sh
@@ -12,12 +12,12 @@ mkdir -p DP_Tcf1
 star (){
   STAR --runMode alignReads --runThreadN 40 --alignIntronMax 1 --outFilterMultimapNmax 1 --genomeDir /mnt/data0/John/genome_index --readFilesIn $fastq --outSAMtype BAM SortedByCoordinate --outWigType wiggle
 }
-cd CD8_Tcf1
+cd $datadir/Tcf1_ChIP_seq/CD8_Tcf1
 fastq-dump SRR1024054
 fastq=SRR1024054
 star
 
-cd CD8_IgG
+cd $datadir/Tcf1_ChIP_seq/CD8_IgG
 fastq-dump SRR1024055
 fastq=SRR1024055
 star
