4faccfb4296ab49b4fbd011a38139042c871e0e4
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-07 11:20:43 -0400

    STAR Alignment Added

---
+++ chip-seq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+datadir=/mnt/data0/ncbi
+cd $datadir
+mkdir -p TCF1_ChIP_seq
+cd TCF1_ChIP_seq
+mkdir -p CD8_Tcf1
+mkdir -p CD8_IgG
+mkdir -p Thy_Tcf1
+mkdir -p Thy_Input
+mkdir -p DP_Tcf1
+##Defining STAR alignment function
+star (){
+  STAR --runMode alignReads --runThreadN 40 --alignIntronMax 1 --outFilterMultimapNmax 1 --genomeDir /mnt/data0/John/genome_index --readFilesIn $fastq --outSAMtype BAM SortedByCoordinate --outWigType wiggle·
+}
+cd CD8_Tcf1
+fastq-dump SRR1024054
+fastq=SRR1024054
+star
+
+cd CD8_IgG
+fastq-dump SRR1024055
+fastq=SRR1024055
+star
+
