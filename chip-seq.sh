5bc5193e4f7bb7b1de6a296d0670c5255b170aaa
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-07 12:05:39 -0400

    added rest of fastq

--- chip-seq.sh
+++ chip-seq.sh
@@ -10,7 +10,7 @@ mkdir -p Thy_Input
 mkdir -p DP_Tcf1
 ##Defining STAR alignment function
 star (){
-  STAR --runMode alignReads --runThreadN 40 --alignIntronMax 1 --outFilterMultimapNmax 1 --genomeDir /mnt/data0/John/genome_index --readFilesIn $fastq --outSAMtype BAM SortedByCoordinate
+  STAR --runMode alignReads --runThreadN 40 --alignIntronMax 1 --outFilterMultimapNmax 1 --genomeDir /mnt/data0/John/GRCh38.81.mm --readFilesIn $fastq --outSAMtype BAM SortedByCoordinate
 }
 cd $datadir/Tcf1_ChIP_seq/CD8_Tcf1
 fastq-dump SRR1024054
@@ -22,3 +22,17 @@ fastq-dump SRR1024055
 fastq=SRR1024055.fastq
 star
 
+cd $datadir/Tcf1_ChIP_seq/Thy_Tcf1
+fastq-dump SRR846897
+fastq=SRR846897.fastq
+star
+
+cd $datadir/Tcf1_ChIP_seq/Thy_Input
+fastq-dump SRR846898
+fastq=SRR846898.fastq
+star
+
+cd $datadir/Tcf1_ChIP_seq/DP_Tcf1
+fastq-dump SRR1685994
+fastq=SRR1685994.fastq
+star
