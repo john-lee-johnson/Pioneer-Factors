9c585f27dff093fb7e27708da96b281c7743d9b6
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-11 17:37:34 -0400

    files.txt

--- fastq-align.sh
+++ fastq-align.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-for i in `ls -d */`;
-do
-	filename=`echo "$i" | rev | cut -d'/' -f2 | rev`		 ##Generates filename variable
-	cd $filename
-	fastq=`ls *.fastq`
-#	STAR --runMode alignReads --runThreadN 40 --genomeDir /mnt/data0/John/mm9.M6.index --readFilesIn $fastq --outSAMtype BAM SortedByCoordinate
-	mv Aligned.sortedByCoord.out.bam $filename.bam
-	cd ..
-	#tail -n +2 $i > ${filename}_temp.bed 		##Removes the trackline from bedgraph file
-done
+while read line; do ##Lists the ATAC seq bam files to do peaking calling
+srr=$(echo $line | cut -d' ' -f1)
+filename=$(echo $line | cut -d' ' -f2)
+echo $srr
+echo $filename
+#STAR --runMode alignReads --runThreadN 40 --genomeDir /mnt/data0/John/genome_index --readFilesIn ${fastq}.fastq --outSAMtype BAM SortedByCoordinate
+Aligned.sortedByCoord.out.bam $filename.bam
+done < fastq_files.txt
