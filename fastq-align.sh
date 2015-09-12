6ebb82644466921a0019d0199d4ab9feae0cb030
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-12 14:29:56 -0400

    fast-q align

--- fastq-align.sh
+++ fastq-align.sh
@@ -1,21 +1,90 @@
 #!/bin/bash
 dir=/mnt/data1/John/pioneer/data
-while read line; do ##Lists the ATAC seq bam files to do peaking calling
+
+function star_generate {
+STAR --runMode genomeGenerate --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38_81 --genomeFastaFiles /mnt/data0/John/Mus_musculus.GRCm38.dna.primary_assembly.fa --sjdbGTFfile /mnt/data0/John/Mus_musculus.GRCm38.81.gtf
+}
+
+function star_chip {
+STAR --runMode alignReads --alignIntronMax 1 --seedSearchStartLmax 30 --outFilterMultimapNmax 2 --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38_81 --readFilesIn ${fastq}.gz --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate
+mv Aligned.sortedByCoord.out.bam ${filename}.bam
+}
+
+function star_rna {
+STAR --runMode alignReads --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38_81 --readFilesIn ${fastq}.gz --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate
+mv Aligned.sortedByCoord.out.bam ${filename}.bam
+}
+
+while read line; do
 srr=$(echo $line | cut -d' ' -f1)
 seq=$(echo $line | cut -d' ' -f2 | cut -d'_' -f1)
 cell=$(echo $line | cut -d' ' -f2 | cut -d'_' -f2)
-if [[ "$seq" = ATAC ]]
-then
+if [[ "$seq" = ATAC ]]; then
+  cd $dir/atac_seq
+  mkdir -p $cell
+  cd $cell
+  #fastq-dump $srr
   echo $srr $seq $cell
 fi
+cd $dir
 if [[ "$seq" = H3* ]]
 then
-  echo $srr $seq $cell
+  cd $dir/chip_seq
+  mkdir -p $cell
+  cd $cell
+  mkdir -p $seq
+  cd $seq
+  #fastq-dump $srr
 fi
+cd $dir
 if [[ "$seq" = RNA ]]
 then
+  cd $dir/rna_seq
+  mkdir -p $cell
+  cd $cell
+  #fastq-dump $srr
   echo $srr $seq $cell
 fi
-#STAR --runMode alignReads --runThreadN 40 --genomeDir /mnt/data0/John/genome_index --readFilesIn ${fastq}.fastq --outSAMtype BAM SortedByCoordinate
-#Aligned.sortedByCoord.out.bam $filename.bam
+cd $dir
 done < files/srr_files.txt
+
+cd /mnt/data1/John/Pioneer-Factors
+while read line; do
+seq=$(echo $line | cut -d' ' -f1)
+cell=$(echo $line | cut -d' ' -f2)
+if [[ "$seq" = ATAC ]]; then
+  cd $dir/atac_seq/$cell
+  #cat SRR*.fastq > ${cell}_${seq}.fastq
+  fastq=${cell}_${seq}.fastq
+  filename=${cell}_${seq}
+  #pigz ${fastq}
+  #star_chip
+  rm SRR*.fastq
+  echo $seq $cell
+fi
+cd $dir
+if [[ "$seq" = H3* ]]
+then
+  cd $dir/chip_seq/$cell/$seq
+  #cat SRR*.fastq > ${cell}_${seq}.fastq
+  fastq=${cell}_${seq}.fastq
+  filename=${cell}_${seq}
+  #pigz ${fastq}
+  star_chip
+  rm SRR*.fastq
+  echo $seq $cell
+fi
+cd $dir
+if [[ "$seq" = RNA ]]
+then
+  cd $dir/rna_seq/$cell
+  #cat SRR*.fastq > ${cell}_${seq}.fastq
+  fastq=${cell}_${seq}.fastq
+  filename=${cell}_${seq}
+  #pigz ${fastq}
+  star_rna
+  rm SRR*.fastq
+  echo $seq $cell
+fi
+cd $dir
+done < files/sample_files.txt
