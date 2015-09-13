a5c7462d92d687433213a10a649c9d6161008cfe
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-13 15:55:31 -0400

    new genome index and macs peak calling parallel

--- fastq-align.sh
+++ fastq-align.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 ##This script will read the SRR files from srr_files.txt, download and convert them to fastq, align them, and output to BAM files
 
-dir=/mnt/data1/John/pioneer/data
+
+dir0=/mnt/data1/John/pioneer
+filedir=/mnt/data1/John/Pioneer-Factors/files
 
 #Makes the directories
-mkdir -p $dir/atac_seq/bam
-mkdir -p $dir/chip_seq/bam
-mkdir -p $dir/rna_seq/bam
-mkdir -p $dir/chip_seq/tcf1/bam
+mkdir -p $dir0/data/atac_seq/bam
+mkdir -p $dir0/data/chip_seq/bam
+mkdir -p $dir0/data/rna_seq/bam
+mkdir -p $dir0/data/chip_seq/tcf1/bam
 
 
-#generates genome index for STAR
+##---------------------SETTING FUNCTIONS---------------------------------------------------
+#STAR generates genome index
 function star_generate {
-STAR --runMode genomeGenerate --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38p4_M6 --genomeFastaFiles /mnt/data0/John/GRCm38.p4.genome.fa --sjdbGTFfile /mnt/data0/John/gencode.vM6.annotation.gtf
+STAR --runMode genomeGenerate --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38p4_M6 --genomeFastaFiles /mnt/data0/John/GRCm38p4_mm10_pa_only.fa --sjdbGTFfile /mnt/data0/John/gencode.vM6.annotation.gtf
 }
 
 #STAR alignment for ChIP-seq and ATAC-seq
@@ -27,6 +30,12 @@ STAR --runMode alignReads --twopassMode Basic --runThreadN 40 --genomeDir /mnt/d
 mv Aligned.sortedByCoord.out.bam ${filename}.bam
 }
 
+#STAR alignment for color space ChIP-seq
+function star_color {
+STAR --runMode alignReads --alignIntronMax 1 --outFilterMultimapNmax 2 --runThreadN 40 --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0 --outFilterMatchNmin 15 --genomeDir /mnt/data0/John/genome_GRCm38p4_M6 --readFilesIn ${fastq}.gz --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate
+mv Aligned.sortedByCoord.out.bam ${filename}.bam
+}
+
 #Read in a file and download the SRR file and convert to fastq
 while read line; do
 srr=$(echo $line | cut -d' ' -f1)
@@ -34,47 +43,55 @@ seq=$(echo $line | cut -d' ' -f2 | cut -d'_' -f1)
 cell=$(echo $line | cut -d' ' -f2 | cut -d'_' -f2)
 if [[ "$seq" = ATAC ]]; then
   echo $srr $seq $cell
-  cd $dir/atac_seq
-  mkdir -p $cell
-  cd $cell
+  cd $dir0/data/atac_seq ; mkdir -p $cell ; cd $cell
   #fastq-dump $srr
 fi
-cd $dir
 if [[ "$seq" = H3* ]]
 then
   echo $srr $seq $cell
-  cd $dir/chip_seq
-  mkdir -p $cell
-  cd $cell
-  mkdir -p $seq
+  cd $dir0/data/chip_seq ; mkdir -p $cell ; cd $cell ; mkdir -p $seq
   cd $seq
   #fastq-dump $srr
 fi
-cd $dir
 if [[ "$seq" = RNA ]]
 then
   echo $srr $seq $cell
-  cd $dir/rna_seq
-  mkdir -p $cell
-  cd $cell
+  cd $dir0/data/rna_seq ; mkdir -p $cell ; cd $cell
   #fastq-dump $srr
 fi
-cd $dir
-done < /mnt/data1/John/Pioneer-Factors/files/srr_files.txt
+done < $filedir/srr_files.txt
 
 #Read in a file and download the SRR file and convert to fastq for Tcf1
+MACSpvalue=1e-7
 while read line; do
-  cd $dir/chip_seq/tcf1
   srr=$(echo $line | cut -d' ' -f1)
+  sample=$(echo $line | cut -d' ' -f2 | cut -d'_' -f1)
   filename=$(echo $line | cut -d' ' -f2)
   fastq=${filename}.fastq
   cell=$(echo $line | cut -d' ' -f2 | cut -d'_' -f2)
-  echo $srr $filename
-  mkdir -p $cell
-  cd $cell
-  fastq-dump $srr && mv SRR*.fastq ${filename}.fastq && pigz ${fastq}
-  star_chip && mv ${filename}.bam $dir/chip_seq/tcf1/bam
-done < /mnt/data1/John/Pioneer-Factors/files/tcf1.txt
+  color=$(echo $line | cut -d' ' -f3)
+  treat=$(echo $line | cut -d' ' -f4)
+  cd $dir0/data/chip_seq/tcf1 ; mkdir -p $cell ; cd $cell ; mkdir -p $sample ; cd $sample
+  if [ $color = "Color" ]; then
+  #fastq-dump -B $srr && mv ${srr}.fastq ${filename}.fastq && pigz ${filename}.fastq
+  star_color && mv ${filename}.bam $dir0/data/chip_seq/tcf1/bam
+  readLength=$(awk 'BEGIN { FS = "|\t" } {if (NR == 7) {print $2}}' Log.final.out)
+    if [ $treat = "Treat" ]; then
+    echo "${dir0}/data/chip_seq/tcf1/bam/${filename}.bam ${filename}_macs ${MACSpvalue} $readLength $control"
+    else
+    control=${dir0}/data/chip_seq/tcf1/bam/${filename}.bam
+    fi
+  else
+  #fastq-dump $srr && mv ${srr}.fastq ${filename}.fastq && pigz ${filename}.fastq
+  star_chip && mv ${filename}.bam $dir0/data/chip_seq/tcf1/bam
+  readLength=$(awk 'BEGIN { FS = "|\t" } {if (NR == 7) {print $2}}' Log.final.out)
+    if [ $treat = "Treat" ]; then
+    echo "${dir0}/data/chip_seq/tcf1/bam/${filename}.bam ${filename}_macs ${MACSpvalue} $readLength $control"
+    else
+    control=${dir0}/data/chip_seq/tcf1/bam/${filename}.bam
+    fi
+  fi
+done < $filedir/tcf1.txt > $filedir/macs_parallel_tcf1.txt
 
 #Combine fastq files, compress to .gz, and align
 cd /mnt/data1/John/Pioneer-Factors
@@ -84,26 +101,25 @@ while read line; do
   fastq=${cell}_${seq}.fastq
   filename=${cell}_${seq}
 if [[ "$seq" = ATAC ]]; then
-  echo $seq $cell
-  cd $dir/atac_seq/$cell
-  #cat SRR*.fastq > ${cell}_${seq}.fastq && pigz ${fastq} && rm SRR*.fastq
-  star_chip && mv ${cell}_${seq}.bam $dir/atac_seq/bam
+  cd $dir0/data/atac_seq/$cell
+  #cat SRR*.fastq > ${filename}.fastq && pigz ${fastq} && rm SRR*.fastq
+  star_chip && mv ${filename}.bam $dir0/data/atac_seq/bam
+  readLength=$(awk 'BEGIN { FS = "|\t" } {if (NR == 7) {print $2}}' Log.final.out)
+  echo "${dir0}/data/atac_seq/bam/${filename}.bam ${filename}_macs ${MACSpvalue} $readLength"
 fi
-cd $dir
+cd $dir0
 if [[ "$seq" = H3* ]]
 then
-  echo $seq $cell
-  cd $dir/chip_seq/$cell/$seq
+  cd $dir0/data/chip_seq/$cell/$seq
   #cat SRR*.fastq > ${cell}_${seq}.fastq && pigz ${fastq} && rm SRR*.fastq
-  star_chip && mv ${cell}_${seq}.bam $dir/atac_seq/bam
+  star_chip && mv ${cell}_${seq}.bam $dir0/data/chip_seq/bam
 fi
-cd $dir
+cd $dir0
 if [[ "$seq" = RNA ]]
 then
-  echo $seq $cell
-  cd $dir/rna_seq/$cell
+  cd $dir0/data/rna_seq/$cell
   #cat SRR*.fastq > ${cell}_${seq}.fastq && pigz ${fastq} && rm SRR*.fastq
-  star_rna && mv ${cell}_${seq}.bam $dir/rna_seq/bam
+  star_rna && mv ${cell}_${seq}.bam $dir0/data/rna_seq/bam
 fi
-cd $dir
-done < /mnt/data1/John/Pioneer-Factors/files/sample_files.txt
+cd $dir0
+done < $filedir/sample_files.txt > $filedir/macs_parallel_atac.txt
--- macs_peak.sh
+++ macs_peak.sh
@@ -4,8 +4,9 @@
 #---------------------Setting Directories-------------------------------------------------
 dir0=/mnt/data1/John/pioneer
 filedir=/mnt/data1/John/Pioneer-Factors/files/
-mkdir -p $dir0/analysis/atac_seq
-cd ${dir0}
+mkdir -p $dir0/analysis/atac_seq/macs
+mkdir -p $dir0/analysis/chip_seq/tcf1
+#cd ${dir0}/data && wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.chrom.sizes
 
 #---------------------MACS PEAK CALLING---------------------------------------------------
 ##This uses MACS to determine ATAC_seq peaks in the provided bam files
@@ -13,33 +14,37 @@ cd ${dir0}
 option=1	#Set to 1 if you want to run MACS peak calling
 #-----------------------------------------------------------------------------------------
 MACSpvalue=1e-7
-cd ${dir0}/analysis/atac_seq/bam
-while read line;	##Lists the ATAC seq bam files to do peaking calling  
-do  	
-	seq=$(echo $line | cut -d' ' -f1)
-	cell=$(echo $line | cut -d' ' -f2)
-	filename=${cell}_${seq}
-	BAMFILE="${filename}.bam"
-	echo "${dir0}/data/atac_seq/bam/${filename} ${filename}_macs ${MACSpvalue}"
-done < $filedir/sample_files.txt > $filedir/macs_parallel.txt
+cd ${dir0}/analysis/atac_seq/macs
 	
-##----------------------MACS Peak Calling--------------------------------------------
-parallel --xapply --dryrun --colsep ' ' -a $filedir/macs_parallel.txt "macs14 -t {1} -n {2} -p {3} -w --single-profile"
-parallel --xapply --colsep ' ' -a $filedir/macs_parallel.txt "macs14 -t {1} -n {2} -p {3} -w --single-profile"
+##----------------------MACS Peak Calling ATAC seq----------------------------------------
+parallel --xapply --dryrun --colsep ' ' -a $filedir/macs_parallel_atac.txt "macs14 -t {1} -n {2} -f BAM -g mm -s {4} -p {3} -w --single-profile"
+#parallel --xapply --colsep ' ' -a $filedir/macs_parallel_atac.txt "macs14 -t {1} -n {2} -f BAM -g mm -s {4} -p {3} -w --single-profile"
+
+##----------------------Generate BigWig file ATAC seq-------------------------------------	
+parallel --xapply --dryrun --colsep ' ' -a $filedir/macs_parallel_atac.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/data/mm10.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig
+parallel --xapply --colsep ' ' -a $filedir/macs_parallel_atac.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/data/mm10.chrom.sizes {2}.bw"	##Converts wig file to BigWig
+
+cd $dir0/analysis/chip_seq/tcf1
+##----------------------MACS Peak Calling Tcf1--------------------------------------------
+parallel --xapply --dryrun --colsep ' ' -a $filedir/macs_parallel_tcf1.txt "macs14 -t {1} -c {5} -n {2} -f BAM -g mm -s {4} -p {3} -w --single-profile"
+#parallel --xapply --colsep ' ' -a $filedir/macs_parallel.txt "macs14 -t {1} -n {2} -p {3} -w --single-profile"
+
+##----------------------Generate BigWig file Tcf1-----------------------------------------	
+parallel --xapply --dryrun --colsep ' ' -a $filedir/macs_parallel_tcf1.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/data/mm10.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig
+#parallel --xapply --dryrun --colsep ' ' -a $filedir/macs_parallel_tcf1.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/data/mm10.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig
 	
 ##----------------------Generate BigWig file-----------------------------------------	
-parallel --xapply --dryrun --colsep ' ' -a ${dir0}/options/macs_parallel.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/Data/mm9.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig
+parallel --xapply --dryrun --colsep ' ' -a $filedir/macs_parallel_atac.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/data/mm10.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig
+#parallel --xapply --colsep ' ' -a ${dir0}/options/macs_parallel.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/data/mm10.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig	
 #parallel --xapply --colsep ' ' -a ${dir0}/options/macs_parallel.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/Data/mm9.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig	
-parallel --xapply --colsep ' ' -a ${dir0}/options/macs_parallel.txt "wigToBigWig -clip {2}_MACS_wiggle/treat/{2}_treat_afterfiting_all.wig.gz ${dir0}/Data/mm9.chrom.sizes {2}_afterfiting_all.bw"	##Converts wig file to BigWig	
-	rm -r *_macsoutput_MACS_wiggle		##Removes MACS output wig directory	
-	end=`date +%s` && runtime=$((end-start)) && echo "Time to complete MACS is " ${runtime} >>  ${dir0}/Time.txt
-	#Getting total number of tags for ATAC-seq peaks used for normalization later
-	cd ${dir0}/Analysis/ATAC_seq
-	rm -f ${dir0}/Analysis/ATAC_seq/read_count.txt
-	for i in `ls *.xls`;			##Lists MACS output xlx file
-	do
-		filename=`echo "$i" | cut -d'.' -f1`
-		reads=`awk 'BEGIN { FS = ":" } {if (NR == 15) {printf "%.5f\n", $2/1000000}}' ${filename}.xls`			##Gets total number of tags and divides by 10^6 and stores it in an array for later
-		cell=`echo $filename | cut -d'_' -f1`
-		echo -e "$cell\t$reads"
-	done >> ${dir0}/Analysis/ATAC_seq/read_count.txt
+#	rm -r *_macsoutput_MACS_wiggle		##Removes MACS output wig directory	
+#	#Getting total number of tags for ATAC-seq peaks used for normalization later
+#	cd ${dir0}/Analysis/ATAC_seq
+#	rm -f ${dir0}/Analysis/ATAC_seq/read_count.txt
+#	for i in `ls *.xls`;			##Lists MACS output xlx file
+#	do
+#		filename=`echo "$i" | cut -d'.' -f1`
+#		reads=`awk 'BEGIN { FS = ":" } {if (NR == 15) {printf "%.5f\n", $2/1000000}}' ${filename}.xls`			##Gets total number of tags and divides by 10^6 and stores it in an array for later
+#		cell=`echo $filename | cut -d'_' -f1`
+#		echo -e "$cell\t$reads"
+#	done >> ${dir0}/Analysis/ATAC_seq/read_count.txt
