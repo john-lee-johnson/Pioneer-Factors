76c3a86a6a9212844f9f0070df457ec831518f27
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-12 20:07:48 -0400

    removed 1_dir.sh

--- 1_dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Makes the directories used for the rest of the scripts
-mkdir -p /mnt/data1/John/pioneer
-mkdir -p /mnt/data1/John/pioneer/data
-mkdir -p /mnt/data1/John/pioneer/data/atac_seq
-mkdir -p /mnt/data1/John/pioneer/data/chip_seq
-mkdir -p /mnt/data1/John/pioneer/data/rna_seq
