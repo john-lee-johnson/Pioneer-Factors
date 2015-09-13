3a283a80190606661a2a0d96e1968757d8c8036c
john-lee-johnson <jljohn@mail.med.upenn.edu>
2015-09-12 20:07:08 -0400

    tcf chip seq

--- fastq-align.sh
+++ fastq-align.sh
@@ -3,21 +3,27 @@
 
 dir=/mnt/data1/John/pioneer/data
 
+#Makes the directories
+mkdir -p $dir/atac_seq/bam
+mkdir -p $dir/chip_seq/bam
+mkdir -p $dir/rna_seq/bam
+mkdir -p $dir/chip_seq/tcf1/bam
+
+
 #generates genome index for STAR
-#change chr names in genome index chrName.txt to chr1, chr2, chr3...
 function star_generate {
-STAR --runMode genomeGenerate --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38_81 --genomeFastaFiles /mnt/data0/John/Mus_musculus.GRCm38.dna.primary_assembly.fa --sjdbGTFfile /mnt/data0/John/Mus_musculus.GRCm38.81.gtf
+STAR --runMode genomeGenerate --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38p4_M6 --genomeFastaFiles /mnt/data0/John/GRCm38.p4.genome.fa --sjdbGTFfile /mnt/data0/John/gencode.vM6.annotation.gtf
 }
 
 #STAR alignment for ChIP-seq and ATAC-seq
 function star_chip {
-STAR --runMode alignReads --alignIntronMax 1 --seedSearchStartLmax 30 --outFilterMultimapNmax 2 --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38_81 --readFilesIn ${fastq}.gz --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate
+STAR --runMode alignReads --alignIntronMax 1 --seedSearchStartLmax 30 --outFilterMultimapNmax 2 --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38p4_M6 --readFilesIn ${fastq}.gz --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate
 mv Aligned.sortedByCoord.out.bam ${filename}.bam
 }
 
 #STAR alignment for RNA-seq 2-pass
 function star_rna {
-STAR --runMode alignReads --twopassMode Basic --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38_81 --readFilesIn ${fastq}.gz --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate
+STAR --runMode alignReads --twopassMode Basic --runThreadN 40 --genomeDir /mnt/data0/John/genome_GRCm38p4_M6 --readFilesIn ${fastq}.gz --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate
 mv Aligned.sortedByCoord.out.bam ${filename}.bam
 }
 
@@ -56,9 +62,21 @@ fi
 cd $dir
 done < files/srr_files.txt
 
-mkdir -p $dir/rna_seq/bam
-mkdir -p $dir/chip_seq/bam
-mkdir -p $dir/atac_seq/bam
+#Read in a file and download the SRR file and convert to fastq for Tcf1
+while read line; do
+  cd $dir/chip_seq/tcf1
+  srr=$(echo $line | cut -d' ' -f1)
+  filename=$(echo $line | cut -d' ' -f2)
+  cell=$(echo $line | cut -d' ' -f2 | cut -d'_' -f2)
+  mkdir -p $cell
+  cd $cell
+  fastq-dump $srr
+  mv SRR*.fastq ${filename}.fastq
+  fastq=${filename}.fastq
+  pigz ${fastq}
+  star_chip
+  mv ${filename}.bam $dir/chip_seq/tcf1/bam
+done < files/tcf1.txt
 
 #Combine fastq files, compress to .gz, and align
 cd /mnt/data1/John/Pioneer-Factors
